bbf8a1a8d03632fc6ecf69665e9a59fe390f5b93
SirajAhmed7 <62938684+SirajAhmed7@users.noreply.github.com>
2023-09-28 00:48:55 +0530

    Fix shop scroll behavior and add shop heading

--- components/shop/shopHeader/ShopHeader.tsx
+++ components/shop/shopHeader/ShopHeader.tsx
@@ -8,6 +8,9 @@ function ShopHeader() {
     <header className="p-4 h-[65vh] lg:h-screen mb-16">
       <div className="relative h-full w-full rounded-[36px] sm:rounded-[48px]">
         <HeaderNav />
+        <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl lg:text-8xl font-extrabold text-white">
+          Shop
+        </h1>
         <ShopHeaderImage />
         {/* <ShopFilters /> */}
       </div>
--- components/ui/ShoeColor.tsx
+++ components/ui/ShoeColor.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import { PiCheckBold } from "react-icons/pi";
 
 type Props = {
@@ -16,20 +15,20 @@ function ShoeColor({ colorCode, name, size }: Props) {
   const searchParams = useSearchParams();
   const borderRadius = size / 4;
 
-  useEffect(() => {
-    // Retrieve scrollY value from localStorage after routing
-    const persistentScroll = localStorage.getItem("persistentScroll");
-    if (persistentScroll === null) return;
+  // useEffect(() => {
+  //   // Retrieve scrollY value from localStorage after routing
+  //   const persistentScroll = localStorage.getItem("persistentScroll");
+  //   if (persistentScroll === null) return;
 
-    // Restore the window's scroll position
-    window.scrollTo({ top: Number(persistentScroll) });
+  //   // Restore the window's scroll position
+  //   window.scrollTo({ top: Number(persistentScroll) });
 
-    // Remove scrollY from localStorage after restoring the scroll position
-    // This hook will run before and after routing happens so this check is
-    // here to make we don't delete scrollY before routing
-    if (Number(persistentScroll) === window.scrollY)
-      localStorage.removeItem("persistentScroll");
-  }, [searchParams]);
+  //   // Remove scrollY from localStorage after restoring the scroll position
+  //   // This hook will run before and after routing happens so this check is
+  //   // here to make we don't delete scrollY before routing
+  //   if (Number(persistentScroll) === window.scrollY)
+  //     localStorage.removeItem("persistentScroll");
+  // }, [searchParams]);
 
   const curColorFilter = searchParams.get("color");
 
@@ -45,10 +44,11 @@ function ShoeColor({ colorCode, name, size }: Props) {
     // or const query = `${'?'.repeat(search.length && 1)}${search}`;
     const query = search ? `?${search}` : "";
 
-    // Save current scrollY value to localStorage before pushing the new route
-    localStorage.setItem("persistentScroll", window.scrollY.toString());
+    // // Save current scrollY value to localStorage before pushing the new route
+    // localStorage.setItem("persistentScroll", window.scrollY.toString());
 
-    router.push(`${pathname}${query}`);
+    router.push(`${pathname}${query}`, { scroll: false });
+    // router.push(`${pathname}${query}`);
   }
 
   return (
