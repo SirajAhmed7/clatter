5c0a8dacc3cefc2abfefb4d4f5baf058c47384b9
SirajAhmed7 <62938684+SirajAhmed7@users.noreply.github.com>
2023-09-28 00:56:39 +0530

    Fix shop header landscape heigth issue

--- components/shop/shopHeader/ShopHeader.tsx
+++ components/shop/shopHeader/ShopHeader.tsx
@@ -3,8 +3,7 @@ import ShopHeaderImage from "./ShopHeaderImage";
 
 function ShopHeader() {
   return (
-    // TODO: change height on landscape
-    <header className="p-4 h-[65vh] lg:h-screen mb-16">
+    <header className="p-4 h-[65vh] md:h-screen mb-16">
       <div className="relative h-full w-full rounded-[36px] sm:rounded-[48px]">
         <HeaderNav />
         <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl lg:text-8xl font-extrabold text-white">
