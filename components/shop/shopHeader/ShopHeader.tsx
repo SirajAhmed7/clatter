fbf75e2dc86a688c54311134ccd06c6a6109f184
SirajAhmed7 <62938684+SirajAhmed7@users.noreply.github.com>
2023-09-28 00:53:30 +0530

    Make PageTransitionWrapper a txt file

--- components/shop/shopHeader/ShopHeader.tsx
+++ components/shop/shopHeader/ShopHeader.tsx
@@ -4,7 +4,6 @@ import ShopHeaderImage from "./ShopHeaderImage";
 function ShopHeader() {
   return (
     // TODO: change height on landscape
-    // TODO: Add heading of Shop
     <header className="p-4 h-[65vh] lg:h-screen mb-16">
       <div className="relative h-full w-full rounded-[36px] sm:rounded-[48px]">
         <HeaderNav />
